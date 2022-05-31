9b47d1b26d5e11f7cd8258afe3c8e90fb10e71e3
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-05-31 16:30:04 -0500

    recon

---
+++ wrangle.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import os
+from env import host, user, password
+
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    It takes in a string name of a database as an argument.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def new_zillow_data():
+    sql_query = """
+            
+SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+FROM properties_2017
+
+LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+
+WHERE propertylandusedesc IN ("Single Family Residential",                       
+                              "Inferred Single Family Residential")"""
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+    
+    return df
+
+def get_zillow_data():
+    '''
+    This function reads in data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('zillow.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('zillow.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame
+        df = new_zillow_data()
+        
+        # Cache data
+        df.to_csv('zillow.csv')
+        
+    return df
+    
+
+###
