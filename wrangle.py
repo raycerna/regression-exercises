f8e8383bdd4b178057ae52474b80845bfa76c4d1
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-06-01 11:22:18 -0500

    updated acquiring

--- wrangle.py
+++ wrangle.py
@@ -11,21 +11,6 @@ def get_connection(db, user=user, host=host, password=password):
     '''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-def new_zillow_data():
-    sql_query = """
-            
-SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
-FROM properties_2017
-
-LEFT JOIN propertylandusetype USING(propertylandusetypeid)
-
-WHERE propertylandusedesc IN ("Single Family Residential",                       
-                              "Inferred Single Family Residential")"""
-    
-    # Read in DataFrame from Codeup db.
-    df = pd.read_sql(sql_query, get_connection('zillow'))
-    
-    return df
 
 def get_zillow_data():
     '''
@@ -46,6 +31,80 @@ def get_zillow_data():
         df.to_csv('zillow.csv')
         
     return df
+
+
+def new_zillow_data():
+    sql_query = """
+            
+SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+FROM properties_2017
+
+LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+
+WHERE propertylandusedesc IN ("Single Family Residential",                       
+                              "Inferred Single Family Residential")"""
     
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+    
+    return df
+
+
+
+def handle_nulls(df):    
+    # We keep 99.41% of the data after dropping nulls
+    # round(df.dropna().shape[0] / df.shape[0], 4) returned .9941
+    df = df.dropna()
+    return df
+
+def handle_rename(df):
+    #renames the columns that are too long
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 'bathroomcnt':'bathrooms', 'calculatedfinishedsquarefeet':'area', 'taxvaluedollarcnt':'tax_value', 'yearbuilt':'year_built'})
+
+    return df
+    
+def optimize_types(df):
+    # Convert some columns to integers
+    # fips, yearbuilt, and bedrooms can be integers
+    df["fips"] = df["fips"].astype(int)
+    df["year_built"] = df["year_built"].astype(int)
+    df["bedrooms"] = df["bedrooms"].astype(int)    
+    df["tax_value"] = df["tax_value"].astype(int)
+    df["area"] = df["area"].astype(int)
+    return df
+
+
+
+def handle_outliers(df):
+    """Manually handle outliers that do not represent properties likely for 99% of buyers and zillow visitors"""
+    df = df[df.bathrooms <= 6]
+    
+    df = df[df.bedrooms <= 6]
+
+    df = df[df.tax_value < 2_000_000]
+
+    return df
+
+def wrangle_zillow():
+    """
+    Acquires Zillow data
+    Handles nulls
+    optimizes or fixes data types
+    handles outliers w/ manual logic
+    returns a clean dataframe
+    """
+    df = get_zillow_data()
+
+    df = handle_nulls(df)
+
+    df = handle_rename(df)
+
+    df = optimize_types(df)
+
+    df = handle_outliers(df)
+
+    df.to_csv("zillow.csv", index=False)
+
+    return df
 
 ###
