3bfe936cee2e73243220bc75ec77be45a56d90c2
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-06-03 21:34:06 -0500

    updated wrangle functions

--- wrangle.py
+++ wrangle.py
@@ -87,6 +87,36 @@ def wrangle_zillow():
 
     return df
 
+def train_validate_test(df):
+    '''
+    Takes a dataframe and splits into train, validate, test 
+    into 70%, 20%, 10% respectively.
+    '''
+
+    # Import to use split function, can only split two at a time
+    from sklearn.model_selection import train_test_split
+
+    # Frist, split into train + validate together and test by itself
+    # Test will be about %10 of the data, train + validate is %70 for now
+    # Set random_state so we can reproduce the same 'random' data
+    train_validate, test = train_test_split(df, test_size = .10, random_state = 123)
+
+    # Second, we plit train + validate into their seperate variables
+    # Train will be about %70 of the data, Validate will be about %20 of the data
+    train, validate = train_test_split(train_validate, test_size = .20, random_state = 123)
+
+    # These two print functions allow us to ensure the date is properly split
+    # Will print the shape of each variable when running the function
+    print("train shape: ", train.shape, ", validate shape: ", validate.shape, ", test shape: ", test.shape)
+
+    # Will print the shape of eachvariable as a percentage of the total data set
+    # Varialbe to hold the sum of all rows (total observations in the data)
+    total = df.count()[0]
+    print("\ntrain percent: ", round(((train.shape[0])/total),2) * 100, 
+            ", validate percent: ", round(((validate.shape[0])/total),2) * 100, 
+            ", test percent: ", round(((test.shape[0])/total),2) * 100)
+
+    return train, validate, test
 
 
 ## TODO Encode categorical variables (and FIPS is a category so Fips to string to one-hot-encoding
