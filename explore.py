29d78cd8f41ca99c321c2015c33829b9ca7cef26
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-06-03 21:33:26 -0500

    updated explore functions

--- explore.py
+++ explore.py
@@ -25,3 +25,29 @@ def plot_categorical_and_continous_vars(categorical_var, continuous_var, df):
     plt.figure()
     plt.figure(figsize=(12,6))
     sns.barplot(x= categorical_var, y= continuous_var, data=df)
+
+def plot_scatter(x,y):
+    f, (ax1) = plt.subplots(1, 1)
+
+    ax1.scatter(x, y)
+    ax1.plot([0, 6000000], [0, 6000000], '--k')
+    ax1.set_ylabel('Target predicted')
+    ax1.set_xlabel('True Target')
+    ax1.set_title('Target vs Predicted')
+    ax1.text(1, 4000000, r'$R^2$=%.2f, MAE=%.2f' % (r2_score(x, y), median_absolute_error(x, y)))
+    ax1.set_xlim([0, 6000000])
+    ax1.set_ylim([0, 6000000])
+    plt.show()
+
+def plot_scatter_log(x,y):
+    f, (ax1) = plt.subplots(1, 1)
+
+    ax1.scatter(x, y)
+    ax1.plot([0, 40], [0, 40], '--k')
+    ax1.set_ylabel('log2 (Target predicted)')
+    ax1.set_xlabel('log2 (True Target)')
+    ax1.set_title('Target vs Predicted')
+    ax1.text(1, 30, r'$R^2$=%.2f, MAE=%.2f' % (r2_score(x, y), median_absolute_error(x, y)))
+    ax1.set_xlim([0, 40])
+    ax1.set_ylim([0, 40])
+    plt.show()
